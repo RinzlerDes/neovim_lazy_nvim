22eb1f19eabd70a8b1596b7cc8282947ac892ea6
rinzler <rinzlerdes@gmail.com>
2024-08-26 17:40:04 -0400

    Added plugin to pass through ',' & ';'

--- lua/plugins/autopairs.lua
+++ lua/plugins/autopairs.lua
@@ -1,6 +1,21 @@
 return {
-    'windwp/nvim-autopairs',
+    "windwp/nvim-autopairs",
     event = "InsertEnter",
-    opts = {} -- this is equalent to setup({}) function
-}
+    config = function()
+        local npairs = require("nvim-autopairs")
+        local Rule = require("nvim-autopairs.rule")
 
+        npairs.setup({})
+
+        for _, punct in pairs { ",", ";" } do
+            npairs.add_rules {
+                Rule("", punct)
+                    :with_move(function(opts) return opts.char == punct end)
+                    :with_pair(function() return false end)
+                    :with_del(function() return false end)
+                    :with_cr(function() return false end)
+                    :use_key(punct)
+            }
+        end
+    end,
+}
